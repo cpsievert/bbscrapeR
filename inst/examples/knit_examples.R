830eaedb4af9b232e5613ac1b781ab2aaedcc1d0
Carson Sievert <cpsievert1@gmail.com>
2015-03-12 00:24:02 -0500

    Add knit_examples

---
+++ inst/examples/knit_examples.R
@@ -0,0 +1,15 @@
+knit_examples <- function() {
+  old <- getwd()
+  on.exit(setwd(old))
+  if (basename(old) != 'examples') stop("basename(getwd()) != 'examples'")
+  dirs <- dir()
+  # keep only directories
+  dirs <- dirs[file_test("-d", dirs)]
+  # navigate intp each example and knit individually
+  for (i in dirs) {
+    setwd(i)
+    e <- try(knitr::knit2html(input = paste0(i, ".Rmd"), envir = new.env()))
+    setwd(old)
+  }
+}
+knit_examples()
