d35dce3a961430cfc66dbbb7ec4036a2466dac76
Carson Sievert <cpsievert1@gmail.com>
2015-03-12 00:10:13 -0500

    Bump version; improve event formatting

---
+++ .push_gh_pages.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+[ "${TRAVIS_PULL_REQUEST}" != "false" ] && exit 0
+
+# remove the examples
+rm -rf examples || exit 0;
+# unzip the package
+for files in '*.tar.gz'; do
+  tar xfz $files
+done
+
+# copy over examples folder and start git repo
+cp -r ../bbscrapeR/inst/examples/ .
+cd examples
+git init
+git config user.name "cpsievert"
+git config user.email "cpsievert1@gmail.com"
+# compile demo html pages and commit
+R -e "devtools::install('../LDAvis'); source('knit_examples.R')"
+git add .
+git commit -m "deployed to github pages"
+
+
+GH_REPO="@github.com/cpsievert/bbscrapeR.git"
+FULL_REPO="https://$GH_TOKEN$GH_REPO"
+git push --force --quiet $FULL_REPO master:gh-pages
--- R/rebound.R
+++ R/rebound.R
@@ -1,6 +1,5 @@
 #' 'Rebound' data from nba.com and wnba.com
 #'
-#'
 #' @param first String specifying the date "YYYYMMDD" to start obtaining game codes.
 #' @param last String specifying the date "YYYYMMDD" to stop obtaining game codes.
 #' @param codes Character vector of game codes with league ID as a prefix
@@ -19,24 +18,25 @@
 #'                leagues = all.leagues, suffix = files)
 #'}
 
-
-
 rebound <- function(first, last, codes, leagues = "nba", suffix="shotchart_all.xml") {
   #translate leagues to '00' for 'nba', '10' for 'wnba', and '20' for 'd'
   tmp <- sub("wnba", "10", leagues)
   tmp <- sub("nba", "00", tmp)
   leaguez <- sub("d", "20", tmp)
   if (missing(codes)) {
+    if (length(first) != 1 || length(last) != 1) 
+      stop("Please provide one 'first' & one 'last' date")
     scrape.env <- environment()
-    data(codes, package="bbscrapeR", envir=scrape.env)
+    data(codes, package = "bbscrapeR", envir = scrape.env)
     ids <- substr(codes, 0, 2)
     codes2 <- codes[ids %in% leaguez]            #keep only codes that match league(s) of interest
     codez <- sub("^[0-9]{2}/", "", codes2)        #get rid of league code
     #datez <- sub("/[A-Z]+", "", codez)          #not needed as as.POSIXct will ignore text
-    datez <- as.POSIXct(codez, format="%Y %m %d")
-    firzt <- as.POSIXct(first, format="%Y %m %d")
-    lazt <- as.POSIXct(last, format="%Y %m %d")
+    datez <- as.POSIXct(codez, format = "%Y %m %d")
+    firzt <- as.POSIXct(first, format = "%Y %m %d")
+    lazt <- as.POSIXct(last, format = "%Y %m %d")
     codex <- codez[firzt <= datez & datez <= lazt]
+    if (length(codex) == 0) stop("Couldn't find any games during that period")
   } else {
     message("Since codes were supplied, the 'first', 'last', and 'leagues' option will be ignored.")
     codex <- codes
@@ -51,7 +51,7 @@ rebound <- function(first, last, codes, leagues = "nba", suffix="shotchart_all.x
   Dprefix <- game.codes[ids == "20"]
   if (length(Dprefix) > 0) Dprefix <- paste0("http://www.nba.com/games/game_component/dynamic/", Dprefix)
   prefixs <- c(NBAprefix, WNBAprefix, Dprefix)
-
+  
   #check for valid inputs
   valid.suffix <- c("boxscore.xml", "pbp_all.xml", "shotchart_all.xml")
   if (!all(suffix %in% valid.suffix)) {
@@ -64,7 +64,9 @@ rebound <- function(first, last, codes, leagues = "nba", suffix="shotchart_all.x
     obs <- XML2Obs(files, as.equiv=TRUE)
     #as with "pbp_all.xml" files, there is no need to add_key since 'message' & 'message//game' observations occur once per file
     tables <- setNames(collapse_obs(obs), paste0("shotchart//", c("message", "game", "event")))
-    tables[["shotchart//event"]] <- format.shotchart.event(tables[["shotchart//event"]])
+    tables[["shotchart//message"]] <- format_message(tables[["shotchart//message"]])
+    tables[["shotchart//game"]] <- format_game(tables[["shotchart//game"]])
+    tables[["shotchart//event"]] <- format_shotchart_event(tables[["shotchart//event"]])
   }
   
   if (any(grepl("boxscore.xml", suffix))) {
@@ -78,6 +80,8 @@ rebound <- function(first, last, codes, leagues = "nba", suffix="shotchart_all.x
                     rename.as = "players", diff.name="home_away", quiet=TRUE)
     table.names <- paste0("boxscore//", c("message", "game", "teams", "officials", "players"))
     tables <- c(tables, setNames(collapse_obs(obs), table.names))
+    tables[["boxscore//message"]] <- format_message(tables[["boxscore//message"]])
+    tables[["boxscore//game"]] <- format_game(tables[["boxscore//game"]])
   }
 
   if (any(grepl("pbp_all.xml", suffix))) {
@@ -86,8 +90,8 @@ rebound <- function(first, last, codes, leagues = "nba", suffix="shotchart_all.x
     female <- grepl("wnba", files)
     wnba.files <- files[female]
     nba.files <- files[!female]
-    wnba.docs <- urlsToDocs(wnba.files)
-    nba.docs <- pbpToDocs(nba.files)
+    wnba.docs <- if (length(wnba.files)) urlsToDocs(wnba.files) else NULL
+    nba.docs <- if (length(nba.files)) pbpToDocs(nba.files) else NULL
     docs <- c(nba.docs, wnba.docs)
     #This is essentially copy-pasted from the guts of XML2Obs
     valid.urls <- sapply(docs, function(x) attr(x, "XMLsource"))
@@ -104,7 +108,11 @@ rebound <- function(first, last, codes, leagues = "nba", suffix="shotchart_all.x
     nms[nms %in% "attrs"] <- "message"
     obs <- setNames(obs, nms)
     table.names <- paste0("pbp//", c("message", "game", "event"))
-    tables <- c(tables, setNames(collapse_obs(obs), table.names))
+    tabs <- setNames(collapse_obs(obs), table.names)
+    tabs[["pbp//message"]] <- format_message(tabs[["pbp//message"]])
+    tabs[["pbp//game"]] <- format_game(tabs[["pbp//game"]])
+    tabs[["pbp//event"]] <- format_pbp_event(tabs[["pbp//event"]])
+    tables <- c(tables, tabs)
   }
   return(tables)
 }
@@ -173,15 +181,62 @@ pbpToDocs <- function(urls, quiet=FALSE){
   return(docs)
 }
 
-
 # Series of formatting functions to coerce variables to their proper (numeric) types
-format.table <- function(dat, nums) {
+format_table <- function(dat, nums) {
   dat <- data.frame(dat, stringsAsFactors=FALSE)
   numz <- nums[nums %in% names(dat)] #error handling (just in case one of the columns doesn't exist)
   for (i in numz) dat[, i] <- as.numeric(dat[, i])
-  return(dat)
+  dat$gcd <- stringr::str_extract(dat$url, "[0-9]{8}/[A-Z]{6}")
+  dat$date <- as.POSIXct(stringr::str_extract(dat$gcd, "[0-9]{8}"), format = "%Y %m %d")   
+  dat
 }
-format.shotchart.event <- function(dat) {
-  event.nums <- c("x", "y")
-  format.table(dat, nums=event.nums)
+# message tables from boxscores and shotcharts seem to be similar
+format_message <- function(dat) {
+  message.nums <- "id"
+  dat <- format_table(dat, nums=message.nums)
+  dat <- dat[,!grepl("gdt", names(dat), fixed = TRUE)]
+  dat
+}
+# game tables from boxscores and shotcharts seem to be similar
+format_game <- function(dat) {
+  message.nums <- c("id", "tie", "ldchg", "pord", "timh", "timloc", "timv",
+                    "gtyp", "prd", "seq", "timet")
+  dat <- format_table(dat, nums=message.nums)
+  dat <- dat[,!grepl("gdt", names(dat), fixed = TRUE)]
+  dat
+}
+format_shotchart_event <- function(dat) {
+  sp <- strsplit(dat[,"pid"], "\\|")
+  dat <- cbind(dat, 
+               player_id = sapply(sp, "[[", 1),
+               player_code = sapply(sp, "[[", 2))
+  matchup <- stringr::str_extract(dat[, "url"], "[A-Z]{6}")
+  dat <- cbind(dat, home_team = substr(matchup, 0, 3),
+               away_team = substr(matchup, 4, 6))
+  dat <- dat[, !grepl("pid", colnames(dat), fixed = TRUE)]
+  event.nums <- c("x", "y", "pts", "time", "player_num")
+  dat <- format_table(dat, nums=event.nums)
+  # in order to match names with pbp event
+  names(dat) <- sub("^id$", "eventid", names(dat))
+  dat
+}
+format_pbp_event <- function(dat) {
+  # strip the game clock since it's in another column
+  desc <- sub("^\\([0-9]{2}:[0-9]{2}[.]?[0-9]*\\)", "", dat[,"XML_value"])
+  # strip team/score info since that is also in other columns
+  desc <- sub("^\\s+", "", (sub("^\\[.*\\]", "", desc)))
+  # strip player name and msg_type for similar reasons
+  #desc <- sub("^[A-Za-z]+\\s+[A-Za-z]+[\\s+]?:", "", desc)
+  dat <- dat[, !grepl("XML_value", colnames(dat), fixed = TRUE)]
+  dat <- cbind(dat, desc)
+  pbp.nums <- c("htms", "vtms", "prd")
+  dat <- format_table(dat, nums = pbp.nums)
+  # translate message/action codes into english
+  data(msgs, package = "bbscrapeR")
+  data(actions, package = "bbscrapeR")
+  dat <- plyr::join(dat, msgs, by = c("msg_type"))
+  dat <- plyr::join(dat, actions, by = c("action_type", "msg_type"))
+  dat <- dat[,!grepl("action_type", names(dat), fixed = TRUE)]
+  dat <- dat[,!grepl("msg_type", names(dat), fixed = TRUE)]
+  dat
 }
